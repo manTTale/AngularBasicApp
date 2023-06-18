a44f380fc56b18a0d81b478893dc29cb5f5107e3
manTTale <mantale.roberto1@yahoo.com>
2023-06-18 19:31:23 +0300

    played with the main component

--- my-first-angular-projet/src/app/app.component.ts
+++ my-first-angular-projet/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-projet';
+  title = 'This is my first App';
+  firstName ='Roberto';
+  lastName='Mantale';
 }
